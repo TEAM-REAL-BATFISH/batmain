e7602999bca8d512f5e2ce93b702c89770daa6a1
Stephen Yang <stephen.yang98@gmail.com>
2024-04-06 16:19:38 -0700

    updated server

--- scratch project/server/server.js
+++ scratch project/server/server.js
@@ -9,7 +9,10 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(cookieParser());
 
 const currentDir = new URL('.', import.meta.url).pathname;
