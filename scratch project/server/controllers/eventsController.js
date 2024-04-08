a075155861f2f35c286b5d76ca01cc2c85d91529
Haider Ali <haiderali@Haiders-MacBook-Air.local>
2024-04-08 12:00:54 -0700

    controllers for events configuration complete

--- scratch project/server/controllers/eventsController.js
+++ scratch project/server/controllers/eventsController.js
@@ -23,13 +23,93 @@ eventsController.getAllEvents = async (req, res, next) => {
   }
 }
 
+eventsController.getHostEvents = async (req, res, next) => {
+  try {
+    const user_id = req.cookies.ssid;
+    const values = [user_id];
+    const query = `SELECT *
+                   FROM events
+                   WHERE host_id = $1`
+    
+    const response = await db(query, values);
+    res.locals.hostEvents = response.rows;
+    return next();
+  } catch(error) {
+    next({
+      log: 'Error retrieving host events',
+      message: {error: 'Error retrieving host events'}
+    })
+  }
+}
+
+eventsController.getAttendingEvents = async (req, res, next) => {
+  try {
+    const user_id = req.cookies.ssid;
+    const values = [user_id];
+    const query = `SELECT *
+                   FROM events_attending
+                   WHERE user_id = $1`
+
+    const response = await db(query, values);
+    res.locals.attendingEvents = response.rows
+    return next();
+  } catch(error) {
+    next({
+      log: 'Error retrieving events that user will attend',
+      message: {error: 'Error retrieving events that user will attend'}
+    })
+  }
+}
+
+eventsController.markGoing = async (req, res, next) => {
+  
+  const { event_id } = req.body;
+  const user_id = req.cookies.ssid;
+  const values = [user_id, event_id]
+
+  try {
+    const query = `INSERT INTO events_attending (user_id, event_id)
+                   VALUES ($1, $2)`
+    await db(query, values);
+    return next();
+  } catch (error) {
+      next({
+        log: 'Error rsvpsing',
+        message: {error: 'Error rsvping'}
+      })
+  }
+}
+
+eventsController.markNotGoing = async (req, res, next) => {
+  
+  const {event_id} = req.body;
+  const user_id = req.cookies.ssid;
+  const values = [user_id, event_id]
+
+  try {
+    const query = `DELETE FROM events_attending
+                   WHERE event_id = $2 AND user_id = $1`
+    await db(query, values);
+    return next();
+  } catch (error) {
+    next({
+      log: 'Error un-rsvpsing',
+      message: {error: 'Error un-rsvping'}
+    })
+  }
+}
 
 // need to add host_id param to addEvent
-eventsController.addEvent = async (res, req, next) => {
+eventsController.addEvent = async (req, res, next) => {
   
-  const { event_title, event_description, location, date} = req.body;
+  const { event_title, event_description, location, date, time} = req.body;
+  const user_id = req.cookies.ssid;
 
-  if (!event_title || !event_description, !location || !date) {
+  // Concatenate date and time strings and create Date object
+  const dateTimeString = `${date} ${time}`;
+  const dateTimeObject = new Date(dateTimeString);
+
+  if (!event_title || !event_description || !location || !date) {
     res.locals.invalidEvent = true;
     return next({
       log: 'invalid inputs on addEvent method',
@@ -38,13 +118,19 @@ eventsController.addEvent = async (res, req, next) => {
   }
 
   try {
-    const values = [event_title, event_description, location, date];
+    const values = [event_title, event_description, location, dateTimeObject, user_id];
 
-    const query = `INSERT INTO events (event_title, event_description, location, date)
-                   VALUES ($1, $2, $3, $4)`;
-
+    const query = `INSERT INTO events (event_title, event_description, location, date, host_id)
+                   VALUES ($1, $2, $3, $4, $5)
+                   RETURNING id`;    
     const result = await db(query, values);
-    res.locals.newEvent = result;
+    res.locals.newEvent = result.rows[0].id;
+
+    const values2 = [user_id, res.locals.newEvent];
+    const query2 = `INSERT INTO events_attending (user_id, event_id)
+                    VALUES ($1, $2)`;           
+    await db(query2, values2);
+
     return next();
   } catch (error) {
     next({
@@ -63,8 +149,13 @@ eventsController.deleteEvent = async (req, res, next) => {
     
     const query = `DELETE FROM events
                    WHERE id = $1`;
+
+    const query2 = `DELETE FROM events_attending
+                    WHERE event_id = $1`
     
     const result = await db(query, values);
+    await db(query2, values);
+
     res.locals.deletedEvent = result;
     return next();
     } catch (error) {
--- scratch project/server/routers/eventRouter.js
+++ scratch project/server/routers/eventRouter.js
@@ -4,10 +4,30 @@ import eventsController from '../controllers/eventsController.js'
 
 const eventRouter = express.Router();
 
+// routes to render main page
 eventRouter.get('/', eventsController.getAllEvents, (req, res) => {
     res.status(200).json(res.locals.events);
+});
+
+eventRouter.get('/myevents', eventsController.getHostEvents, (req, res) => {
+    res.status(200).json(res.locals.hostEvents);
 })
 
+eventRouter.get('/upcomingevents', eventsController.getAttendingEvents, (req, res) => {
+    res.status(200).json(res.locals.attendingEvents);
+})
+
+// routes for rsvp
+eventRouter.post('/going', eventsController.markGoing, (req, res,) => {
+    res.status(200);
+})
+
+eventRouter.post('/unattend', eventsController.markNotGoing, (req, res) => {
+    res.status(200);
+})
+
+
+// routes for host
 eventRouter.post('/add', eventsController.addEvent, (req, res) => {
     res.status(200).json(res.locals.newEvent);
 });
@@ -26,4 +46,4 @@ export default eventRouter;
 //for update and delete, make sure that the user is host
 
 //add myevents
-//add 
+//add
